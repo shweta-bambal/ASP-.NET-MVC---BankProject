37e5cbb0cbc53a59829ac874c4523887fddc333b
Shweta Bambal <75642571+shweta-bambal@users.noreply.github.com>
2021-07-21 22:17:00 +0530

    Add files via upload

---
+++ ViewModels/CreateTransactionViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BankProject.Models;
+
+namespace BankProject.ViewModels
+{
+    public class CreateTransactionViewModel
+    {
+        public AllTransactions AllTransactions { get; set; }
+        public Customer Customers { get; set; }
+        public IEnumerable<Transaction> Transactions { get; set; }
+    }
+}
